98925bc3eee07e57989ede55bc7b4dc116dbdbb4
이지민 <beb4ljm@gmail.com>
2022-08-14 13:39:00 +0900

    Add [prime make]

---
+++ programers/prime_number_make.js
@@ -0,0 +1,32 @@
+//소수 만들기
+function solution(nums) {
+  var answer = 0;
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        console.log(nums[i], nums[j], nums[k]);
+        if (checkPrime(nums[i] + nums[j] + nums[k])) answer++;
+      }
+    }
+  }
+
+  return answer;
+}
+
+const checkPrime = num => {
+  console.log(num);
+  for (let i = 2; i <= num / 2; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+// let nums = [1, 2, 3, 4];
+//1
+
+let nums = [1, 2, 7, 6, 4];
+//4
+//["Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."]
+const result = solution(nums);
+console.log('최종 결과', result);
