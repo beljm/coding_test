3efb53f920afe068953436a3e02f4aadc4be52c2
이지민 <beb4ljm@gmail.com>
2022-08-30 23:56:27 +0900

    Add [target number]

--- programers/make_tow_queue_look_like_same.js
+++ programers/make_tow_queue_look_like_same.js
@@ -7,9 +7,6 @@ function solution(queue1, queue2) {
   let len = queue1.length;
   let index1 = 0;
   let index2 = 0;
-  if (q1Sum === q2Sum) {
-    return 0;
-  }
   for (let i = 0; i < len * 3; i++) {
     console.log(q1Sum, q2Sum, harf);
     if (q1Sum < harf) {
@@ -58,63 +55,5 @@ let queue2 = [1, 1, 1, 9, 1];
 
 const result = solution(queue1, queue2);
 console.log('결과:', result);
-
-// 두 큐 합 같게 만들기
-// function solution(queue1, queue2) {
-//     let arr = [...queue1, ...queue2];
-//     let harf = calSum(arr) / 2;
-//     let result = [];
-//     let answer = -1;
-//     let len = queue1.length;
-//     for (let i = 0; i < arr.length; i++) {
-//       let sum = 0;
-//       let count = 0;
-//       for (let j of arr) {
-//         sum += j;
-//         count++;
-//         if (sum > harf) {
-//           count = len * 2;
-//           break;
-//         }
-//         if (sum === harf) {
-//           break;
-//         }
-//       }
-//       result.push(count);
-//       let temp = arr.slice(0);
-//       arr = [...temp.slice(1), temp[0]];
-//     }
-//     console.log(result);
-//     for (let i = 0; i < result.length; i++) {
-//       if (result[i] !== arr.length) {
-//         console.log('i', result[i], i);
-//         if (result[i] + (i % len) === len) {
-//           console.log('11111');
-//           answer === -1 || answer > i % len ? (answer = i % len) : null;
-//         } else if (result[i] + (i % len) > len) {
-//           console.log('22222');
-//           answer === -1 || answer > result[i] - (len - (i % len)) + (i % len)
-//             ? (answer = result[i] - (len - (i % len)) + (i % len))
-//             : null;
-//         } else {
-//           console.log('33333');
-//           answer === -1 || answer > result[i] + 2 * (i % len) + len
-//             ? (answer = result[i] + 2 * (i % len) + len)
-//             : null;
-//         }
-//       }
-//       console.log(answer);
-//     }
-
-//     return answer;
-//   }
-
-//   const calSum = arr => {
-//     let sum = arr.reduce((acc, cur) => {
-//       acc += cur;
-//       return acc;
-//     }, 0);
-//     return sum;
-//   };
 
 //good code
---
+++ programers/target_number.js
@@ -0,0 +1,25 @@
+//타겟 넘버
+function solution(numbers, target) {
+  var answer = 0;
+  console.log('==============');
+  console.log(numbers, target);
+  if (numbers.length <= 1) {
+    return Math.abs(target) === numbers[0] ? 1 : 0;
+  }
+  let num = numbers[0];
+  answer += solution(numbers.slice(1), target + num);
+  answer += solution(numbers.slice(1), target - num);
+
+  return answer;
+}
+
+// let numbers = [1, 1, 1, 1, 1];
+// let target = 3;
+// 5
+
+let numbers = [4, 1, 2, 1];
+let target = 4;
+// 2
+
+let result = solution(numbers, target);
+console.log('결과:', result);
