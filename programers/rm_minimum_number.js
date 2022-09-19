60c7e918522d6346d4f510b1be9f8cb08e9565c5
이지민 <beb4ljm@gmail.com>
2022-09-19 18:45:54 +0900

    Add [remove minimum number2]

--- programers/rm_minimum_number.js
+++ programers/rm_minimum_number.js
@@ -1,6 +1,6 @@
 //제일 작은 수 제거하기
 function solution(arr) {
-  if (arr.length === 1) return -1;
+  if (arr.length === 1) return [-1];
   let minimum = Math.min(...arr);
   let point = arr.indexOf(minimum);
   return [...arr.slice(0, point), ...arr.slice(point + 1, arr.length)];
@@ -11,3 +11,10 @@ let arr = [4, 3, 2, 1];
 
 let result = solution(arr);
 console.log('출력:', result);
+
+//good code
+// function solution(arr) {
+//   arr.splice(arr.indexOf(Math.min(...arr)), 1);
+//   if (arr.length < 1) return [-1];
+//   return arr;
+// }
