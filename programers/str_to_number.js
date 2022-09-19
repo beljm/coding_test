ace0d6019380316af714307fe442195450903d1e
이지민 <beb4ljm@gmail.com>
2022-09-19 17:21:12 +0900

    Add [str to number]

---
+++ programers/str_to_number.js
@@ -0,0 +1,13 @@
+//문자열을 정수로 바꾸기
+function solution(s) {
+  return Number(s);
+}
+
+// let s = '1234';
+// 1234
+
+let s = '-1234';
+// -1234
+
+let result = solution(s);
+console.log('결과:', result);
