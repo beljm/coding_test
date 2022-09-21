190e8453b35dde5b66ffba4ee71ceb8ee2283d98
이지민 <beb4ljm@gmail.com>
2022-09-21 15:01:21 +0900

    Add [get center char]

---
+++ programers/get_center_chat.js
@@ -0,0 +1,15 @@
+//가운데 글자 가져오기
+function solution(s) {
+  return s.length % 2 === 0
+    ? s.slice(s.length / 2 - 1, s.length / 2 + 1)
+    : s[Math.round(s.length / 2 - 1)];
+}
+
+// let s = 'abcde';
+//"c"
+
+let s = 'qwer';
+//"we"
+
+const result = solution(s);
+console.log('출력:', result);
