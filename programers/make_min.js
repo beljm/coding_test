601f84d126b93cc8ee81d1f751de88c81a1ba922
beljm <beb4ljm@gmail.com>
2022-09-22 16:33:44 +0900

    Add [make min]

---
+++ programers/make_min.js
@@ -0,0 +1,21 @@
+//최솟값 만들기
+function solution(A,B){
+    A = A.sort((a,b)=>a-b);
+    B = B.sort((a,b)=>b-a);
+    let minSum = 0
+    for(let i = 0; i < A.length; i++){
+        minSum += A[i]*B[i];
+    }
+    return minSum
+}
+
+// let A = [1, 4, 2];
+// let B = [5, 4, 4];
+// 29
+
+let A = [1, 2];
+let B = [3, 4];
+// 10
+
+const result = solution(A, B);
+console.log('결과:', result);
