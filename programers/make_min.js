27398e02a5e96bfc5aa2c6c7e8bf4cd7b0360bbb
이지민 <beb4ljm@gmail.com>
2022-09-23 22:36:29 +0900

    Add [make num sum]

--- programers/make_min.js
+++ programers/make_min.js
@@ -1,12 +1,12 @@
 //최솟값 만들기
-function solution(A,B){
-    A = A.sort((a,b)=>a-b);
-    B = B.sort((a,b)=>b-a);
-    let minSum = 0
-    for(let i = 0; i < A.length; i++){
-        minSum += A[i]*B[i];
-    }
-    return minSum
+function solution(A, B) {
+  A = A.sort((a, b) => a - b);
+  B = B.sort((a, b) => b - a);
+  let minSum = 0;
+  for (let i = 0; i < A.length; i++) {
+    minSum += A[i] * B[i];
+  }
+  return minSum;
 }
 
 // let A = [1, 4, 2];
