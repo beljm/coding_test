b4c02dca636f83876cfa1cc31116de8a166d5516
이지민 <beb4ljm@gmail.com>
2022-09-27 01:05:24 +0900

    Add [matrix multiplication]

--- programers/matrices_sum.js
+++ programers/matrices_sum.js
@@ -1,18 +1,24 @@
 //행렬 덧셈
 function solution(arr1, arr2) {
-    let answer = [];
-    for(let i = 0; i < arr1.length; i++){
-        let arr = [];
-        for(let j = 0; j < arr1[i].length; j++){
-            arr.push(arr1[i][j]+arr2[i][j])
-        }
-        answer.push(arr);
-    }    
-    return answer;
+  let answer = [];
+  for (let i = 0; i < arr1.length; i++) {
+    let arr = [];
+    for (let j = 0; j < arr1[i].length; j++) {
+      arr.push(arr1[i][j] + arr2[i][j]);
+    }
+    answer.push(arr);
+  }
+  return answer;
 }
 
-let arr1 = [[1,2],[2,3]];
-let arr2 = [[3,4],[5,6]];
+let arr1 = [
+  [1, 2],
+  [2, 3],
+];
+let arr2 = [
+  [3, 4],
+  [5, 6],
+];
 
 const result = solution(arr1, arr2);
 console.log('결과:', result);
